52e0214528c05bb3974841c00b19443efb46dd4f
MrAmarok <marc.mendia@gmail.com>
2026-04-21 11:48:06 +0200

    chore: update fetch in twitch loop to counter restart at error

--- app/src/loop.ts
+++ app/src/loop.ts
@@ -12,70 +12,79 @@ export async function twitchCallLoop(client: Client) {
   states.streamInfos = states.streamInfos.filter((info) => {
     return twitchInfo.some((s) => s.username === info.username);
   });
+  
   const results = await Promise.all(
     twitchInfo.map(async (info) => {
-      const res = await fetch(
-        `https://api.twitch.tv/helix/streams?user_login=${info.username}`,
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${userSessions.twitchToken}`,
-            "Client-Id": userSessions.twitchClientId,
+      try {
+        const res = await fetch(
+          `https://api.twitch.tv/helix/streams?user_login=${info.username}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${userSessions.twitchToken}`,
+              "Client-Id": userSessions.twitchClientId,
+            },
           },
-        },
-      );
-      if (!res.ok) {
-        if (res.status === 401) {
-          console.error(
-            `❌ Unauthorized: Twitch token might be expired. Status: ${res.statusText}`,
-          );
-          return false;
-        } else {
-          console.error(
-            `❌ Failed to fetch Twitch stream info for ${info.username}: ${res.statusText}`,
-          );
+        );
+
+        if (!res.ok) {
+          if (res.status === 401) {
+            console.error(
+              `❌ Unauthorized: Twitch token might be expired. Status: ${res.statusText}`,
+            );
+            return false;
+          } else {
+            console.error(
+              `❌ Failed to fetch Twitch stream info for ${info.username}: ${res.statusText}`,
+            );
+            return true;
+          }
+        }
+        
+        const data = await res.json();
+
+        if (data.data && data.data.length === 0 && !info.message_sended) {
+          return true;
+        } else if (data.data && data.data.length === 0 && info.message_sended) {
+          await updateSocialMediaMessageSend(info.username, false);
+          if (
+            states.streamInfos.some((stream) => stream.username === info.username)
+          ) {
+            states.streamInfos = states.streamInfos.filter(
+              (stream) => stream.username !== info.username,
+            );
+          }
+          return true;
+        } else if (data.data && data.data.length > 0 && !info.message_sended) {
+          states.streamInfos.push({
+            username: info.username,
+            link: `https://www.twitch.tv/${info.username}`,
+            title: data.data[0].title,
+          });
+          await updateSocialMediaMessageSend(info.username, true);
+          const message = twitchMessageAnnonce({
+            ...data.data[0],
+            bot_name: client.user?.username || "Slimy Bot",
+          });
+          client.channels.fetch(info.channel_id).then((channel) => {
+            if (channel && channel.isTextBased()) {
+              (channel as TextChannel).send({
+                content: `${info.message}`,
+                embeds: [message],
+              });
+            }
+          });
+
+          console.log(`\n📢 ${info.username} is live! Announcement sent.`);
           return true;
         }
-      }
-      const data = await res.json();
-
-      if (data.data && data.data.length === 0 && !info.message_sended) {
-        return true;
-      } else if (data.data && data.data.length === 0 && info.message_sended) {
-        await updateSocialMediaMessageSend(info.username, false);
-        if (
-          states.streamInfos.some((stream) => stream.username === info.username)
-        ) {
-          states.streamInfos = states.streamInfos.filter(
-            (stream) => stream.username !== info.username,
-          );
-        }
-        return true;
-      } else if (data.data && data.data.length > 0 && !info.message_sended) {
-        states.streamInfos.push({
-          username: info.username,
-          link: `https://www.twitch.tv/${info.username}`,
-          title: data.data[0].title,
-        });
-        await updateSocialMediaMessageSend(info.username, true);
-        const message = twitchMessageAnnonce({
-          ...data.data[0],
-          bot_name: client.user?.username || "Slimy Bot",
-        });
-        client.channels.fetch(info.channel_id).then((channel) => {
-          if (channel && channel.isTextBased()) {
-            (channel as TextChannel).send({
-              content: `${info.message}`,
-              embeds: [message],
-            });
-          }
-        });
-
-        console.log(`\n📢 ${info.username} is live! Announcement sent.`);
-        return true;
+      } catch (error: any) {
+        console.error(`⚠️ Erreur réseau lors de la vérification de ${info.username} :`, error.message);
+        return true; // On retourne true pour continuer la boucle sans déclencher un refresh de token
       }
     }),
   );
+  
   if (results.includes(false)) {
     console.log("\n⚠️ Twitch token might be expired, refreshing...");
     await refreshTwitchToken(userSessions);
